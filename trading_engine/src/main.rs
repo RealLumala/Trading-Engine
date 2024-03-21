ce7a97ace2c236400b7fb489f226bc1b38064e60
Eng. David Lumala <dlumala20@gmail.com>
2024-03-21 10:56:56 +0300

    Add files via upload

---
+++ trading_engine/src/main.rs
@@ -0,0 +1,28 @@
+mod matching_engine;
+use matching_engine::orderbook::{Order, BidOrAsk, Orderbook};
+use matching_engine::engine::{TradingPair, MatchingEngine};
+use rust_decimal_macros::dec;
+
+fn main() {
+    let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
+    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 3.45);
+    let sell_order_from_bob = Order::new(BidOrAsk::Ask, 34.45);
+
+    let mut orderbook = Orderbook::new();
+    orderbook.add_order(dec!(41.4), sell_order_from_bob);
+    orderbook.add_order(dec!(4.4), buy_order_from_bob.clone());
+    orderbook.add_order(dec!(75.4), buy_order_from_bob);
+    orderbook.add_order(dec!(4.4), buy_order_from_alice);
+
+    // println!("{:?}", orderbook);
+    let mut engine = MatchingEngine::new();
+    let trade_pair = TradingPair::new("BTC".to_string(), "USDT".to_string());
+    let alt_trade_pair = TradingPair::new("ETH".to_string(), "EURT".to_string());
+
+    engine.add_new_pair(trade_pair.clone());
+
+    let my_buy_order = Order::new(BidOrAsk::Bid, 4.0);
+
+    engine.place_limit_order(alt_trade_pair, dec!(30000.0), my_buy_order).unwrap_err();
+
+}
